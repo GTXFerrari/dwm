9794a1f211659cd3016ae6feccd39b4ab14ec57d
Jake Ferrari <jakeferrari93@gmail.com>
2022-06-15 19:40:05 -0700

    add emoji support (install libxft-bgra from aur, remove iscol line in drw.c), change main font to adobe source code pro, add joypixels font to config.h

--- config.h
+++ config.h
@@ -11,7 +11,7 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;     /* 0 means no bar */
 static const int topbar             = 1;     /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
+static const char *fonts[]          = { "Source Code Pro Medium:size=14:antialias=true:autohint=true",	"JoyPixels:pixelsize=18:antialias=true:autohint=true" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
@@ -25,7 +25,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "󾠮", "󾠯", "󾠰", "󾠱", "󾠲", "󾠳", "󾠴", "󾠵", "󾠶" };
 
 static const Rule rules[] = {
 	/* xprop(1):

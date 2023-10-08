e409c2decbb0e19d6d58eabb21772df768149a58
Jake Ferrari <jakeferrari93@gmail.com>
2023-10-07 23:19:02 -0700

    add window instances to specific tags

--- config.def.h
+++ config.def.h
@@ -36,8 +36,11 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+  { "Steam",          "Steam",          "Steam",            1 << 8,               0,               0 },
+  { "Steam",          "Steam",         "Friends List",      1 << 8,               1,               0 },                                                                                     
+  { "Steam",          "Steam",         "Steam Settings",      1 << 8,               1,               0 },                                                                                     
+  { "Lutris",           NULL,             NULL,             1 << 8,               1,               0 }, 
+  { "discord",          NULL,             NULL,             1 << 7,               0,               0 },
 };
 
 /* layout(s) */
